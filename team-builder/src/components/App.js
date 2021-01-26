5069aa7ceaa4f1a90779fa78b0a310c291b90d0c
Daniel Gamboa <danielgamboa10@gmail.com>
2021-01-26 14:32:20 -0600

    Import useState and initiate state for a list of team members

---
+++ team-builder/src/components/App.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+export default function App() {
+  const [members, setMembers] = useState([]);
+
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
