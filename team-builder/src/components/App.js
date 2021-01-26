6814fb62c9c057b362d6fc258fafc3ca2347dffd
Daniel Gamboa <danielgamboa10@gmail.com>
2021-01-26 14:56:14 -0600

    Create Form and Member components; populate basic form elements in Form

--- team-builder/src/components/App.js
+++ team-builder/src/components/App.js
@@ -1,11 +1,39 @@
 import React, { useState } from 'react';
+import Member from './Member'
+import Form from './Form'
+
+const initialFormValues = {
+  name: '',
+  email: '',
+  role: ''
+};
 
 export default function App() {
   const [members, setMembers] = useState([]);
+  const [formValues, setFormValues] = useState(initialFormValues);
+
+  const updateForm = (inputName, inputValue) => {
+
+  }
+
+  const submitForm = () => {
+
+  }
 
   return (
-    <div className="App">
-      
+    <div className='App'>
+      <Form 
+        values={formValues}
+        update={updateForm}
+        submit={submitForm}  
+      />
+      {
+        members.map((member, index) => {
+          return (
+            <Member key={index} details={member}/>
+          )
+        })
+      }
     </div>
   );
 }
